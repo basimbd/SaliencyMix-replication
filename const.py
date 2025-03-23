80d8a45a561f2f40d522af5b5f51cfde21abd399
basimbd <basimuddintuhin@gmail.com>
2025-03-23 00:38:21 -0400

    add model test script

---
+++ const.py
@@ -0,0 +1,6 @@
+# CIFAR-10 normalization values: https://github.com/kuangliu/pytorch-cifar/issues/19
+cifar10_mean = (0.4914, 0.4822, 0.4465)
+cifar10_std = (0.2470, 0.2435, 0.2616)
+
+cifar100_mean = (0.5071, 0.4867, 0.4408)
+cifar100_std = (0.2675, 0.2565, 0.2761)
---
+++ evaluate.py
@@ -0,0 +1,37 @@
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+from models.resnet import ResNet50
+from const import cifar10_mean, cifar10_std
+
+def evaluate(model=None, model_path=None, test_loader=None):
+    assert model is not None or model_path is not None, "Either 'model' or 'model_path' should be provided"
+    if test_loader is None:
+        test_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(cifar10_mean, cifar10_std)])
+
+        test_dataset = datasets.CIFAR10(root='data/', train=False, download=True, transform=test_transform)
+        test_loader = DataLoader(test_dataset, batch_size=128, shuffle=False, num_workers=4)
+
+    if model is None:
+        model = ResNet50()
+        model.load_state_dict(torch.load(model_path))
+        model = torch.nn.DataParallel(model).cuda()
+
+    correct = 0
+    total = 0
+    model.eval()
+    with torch.no_grad():
+        for images, labels in test_loader:
+            images = images.cuda()
+            labels = labels.cuda()
+            pred = model(images)
+            _, pred_idxs = torch.max(pred.data, 1)
+            total += labels.size(0)
+            correct += (pred_idxs == labels.data).sum().item()
+    return 100 * correct / total
+
+if __name__ == '__main__':
+    acc = evaluate()
+    print(f"Accuracy: {acc:.2f}%")
