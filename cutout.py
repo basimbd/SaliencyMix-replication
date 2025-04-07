d050fae9148f1e25f617f3e62193fd5746cfc4bc
basimbd <basimuddintuhin@gmail.com>
2025-04-06 21:49:27 -0400

    view cutout image

--- cutout.py
+++ cutout.py
@@ -1,4 +1,6 @@
 import torch
+from PIL import Image
+from torchvision import transforms
 
 class Cutout(object):
     def __init__(self, n_holes, length):
@@ -10,7 +12,7 @@ class Cutout(object):
         height = image.size(1)
         width = image.size(2)
 
-        mask = torch.ones(height, width, dtype=torch.float32)
+        mask = torch.ones(height, width, dtype=torch.float32).cuda()
 
         for n in range(self.n_holes):
             x = torch.randint(0, width, (1,)).item()
@@ -19,9 +21,18 @@ class Cutout(object):
             x1, x2 = max(0, x - self.length // 2), min(width, x + self.length // 2)
             y1, y2 = max(0, y - self.length // 2), min(height, y + self.length // 2)
 
-            mask[y1: y2, x1: x2] = 0.
+            mask[y1:y2, x1:x2] = 0.
 
         mask = mask.expand_as(image)
         image = image * mask
 
         return image
+
+if __name__ == '__main__':
+    cutout = Cutout(1, length=32)
+    image = Image.open("cat-close-up-of-side-profile.jpg")
+    # print(f"(w, h) = {image.size}")
+    image = transforms.ToTensor()(image).unsqueeze(0).cuda()
+    image = cutout(image[0])
+    image = image * 255
+    transforms.ToPILImage()(image.byte()).show()
